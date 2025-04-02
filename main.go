cb8536fb710613ae89953d94d8ba7c6e1ce2512f
Gino Paoletti <gpaolettigeuna@gmail.com>
2025-04-02 15:43:30 -0300

    first endpoints

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
+)
+
+type book struct {
+	ID     string `json:"id,omitempty" binding:"-"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books = []book{
+	{ID: uuid.New().String(), Title: "The Go Programming Language", Author: "Alan Donovan, Brian Kernighan"},
+	{ID: uuid.New().String(), Title: "Docker: Up & Running", Author: "Sean P. Kane"},
+	{ID: uuid.New().String(), Title: "The Kubernetes Book", Author: "Nigel Poulton"},
+}
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
+func postBook(c *gin.Context) {
+	var newBook book
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+	newBook.ID = uuid.New().String()
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.POST("/books", postBook)
+
+	router.Run("localhost:8080")
+}
